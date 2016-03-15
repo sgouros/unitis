b8981e2beeb0d114474a15d4e4092afde12ec21a
sgouros <sgouros@sgouros.gr>
2016-03-15 17:09:01 +0200

    by George Sgouros

--- app/pods/collective-decisions/new/controller.js
+++ app/pods/collective-decisions/new/controller.js
@@ -1,34 +1,27 @@
 import Ember from 'ember';
-import {i,w} from 'unitis/utils/ULogger';
+import {d,i,w,e} from 'unitis/utils/ULogger';
 
 export default Ember.Controller.extend({
 
   actions: {
 
-    addProject(collectiveDecision){
+    addProject(collectiveDecision) {
 
       console.log(collectiveDecision);
 
       let project = this.store.createRecord('project');
-      project.code='a new project code';
+      project.code = 'a new project code';
 
-      i(this,'project just created');
-      
-      if(!collectiveDecision.projects) {
-        i(this,'the cd has no projects');
+      i(this, 'project just created');
+
+      if (!collectiveDecision.projects) {
+        i(this, 'the cd has no projects');
         collectiveDecision.projects = [];
       }
 
-        collectiveDecision.get('projects').pushObject(project);
+      collectiveDecision.get('projects').pushObject(project);
 
-
-        // TODO αυτή τη στιγμή τα projects είναι promises. πρέπει να δω πως γίνεται να γίνονται resolved μεσω του rsvp
-
-
+      // TODO αυτή τη στιγμή τα projects είναι promises. πρέπει να δω πως γίνεται να γίνονται resolved μεσω του rsvp
     }
-
-
   }
-
-
 });
