f21bb5a1c2792b8b30311c065dce266fb2e7c203
George Sgouros <sgouros@sgouros.gr>
2016-03-23 13:59:35 +0200

    δουλεύει ο logger ακόμα και όταν δεν υπάρχει this από τον caller

--- app/pods/collective-decisions/edit/controller.js
+++ app/pods/collective-decisions/edit/controller.js
@@ -5,47 +5,38 @@ export default Ember.Controller.extend({
 
   parentController: Ember.inject.controller('collective-decisions'),
 
+  saveChildProjects: function saveChildProjects(parentDecision) {
+    parentDecision.get('projects').forEach(function(aProject) {
+      promisesArray.push(aProject.save());
+      debug(editCollectiveDecisionsController,'pushing inside editCollectiveDecision');
+    });
+
+    return Ember.RSVP.all(promisesArray).then(function afterPromises() {
+      collectiveDecisionsController.set('responseMessage', message);// aυτό σετάρει τον collective-decisions.edit controller. Εγώ πρέπει να κοιτάω τον collective-decisions controller
+      info(editCollectiveDecisionsController,message);
+      route.transitionTo('collective-decisions');
+    }).catch(function failedToEdit() {
+      error(editCollectiveDecisionsController, 'one of the saves failed inside editCollectiveDecision');
+    });
+
+  },
+
   actions: {
 
     editCollectiveDecision(collectiveDecision) { //ερχεται από το collective-decision-item component
-      debug(this,'editCollectiveDecision called');
-
+      info(this,'editCollectiveDecision called');
       let route   = this.get('target');
       let message = 'EDITED collective decision with id: ' + collectiveDecision.get('id');
       let promisesArray = [];
-      let _this=this;
-      let pc=this.get('parentController');
-
-
-// TODO #11
-      collectiveDecision.save().then((aDecision) => {
-        aDecision.get('projects').forEach(function(aProject) {
-          promisesArray.push(aProject.save());
-          debug(_this,'pushing inside editCollectiveDecision');
-        });
-
-        return Ember.RSVP.all(promisesArray).then(function afterPromises() {
-          pc.set('responseMessage', message);// aυτό σετάρει τον collective-decisions.edit controller. Εγώ πρέπει να κοιτάω τον collective-decisions controller
-          info(_this,message);
-          route.transitionTo('collective-decisions');
-
-
-        }).catch(function failedToEdit() {
-          error(_this, 'one of the saves failed inside editCollectiveDecision');
-        });
-
-      });
+      let editCollectiveDecisionsController = this;
+      let collectiveDecisionsController = this.get('parentController');
+      collectiveDecision.save().then(editCollectiveDecisionsController.saveChildProjects); // TODO #11
     },
-
-
 
     addProject(collectiveDecision) {
       let new_project = this.store.createRecord('project');
-
       new_project.code = 'default project code';
-
       info(this, 'project just created');
-
       if (!collectiveDecision.projects) {
         info(this, 'the cd has no projects');
         collectiveDecision.projects = [];
