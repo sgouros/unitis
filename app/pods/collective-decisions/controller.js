d2b450b64869de7a8bfdc8d4bb2cc8ba03ae77a3
George Sgouros <sgouros@sgouros.gr>
2016-03-24 13:33:02 +0200

    να μην εμφανίζεται το πράσινο για τα messages συνέχεια

--- app/pods/collective-decisions/controller.js
+++ app/pods/collective-decisions/controller.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  responseMessage: 'my default George message'
+  responseMessage: '' // εμφανίζεται στις συλλογικές αποφάσεις σε πράσινο
 });
